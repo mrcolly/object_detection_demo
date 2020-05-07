4721201280cdf4c8542d1ea72c47e5f2cd00fe09
Alessio Colombo <alessio.colombo@openmindonline.it>
2020-05-07 20:31:09 +0200

    added images to test

---
+++ custom_object_detect.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from IPython.display import display
+
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+
+utils_ops.tf = tf.compat.v1
+tf.gfile = tf.io.gfile
+
+
+def load_model(model_name):
+    model_dir = "/Users/alessio.colombo/Documents/projects/python/orangeBallDetectorTensor/trained_inference_graph/saved_model/"
+
+    model = tf.saved_model.load(str(model_dir))
+    model = model.signatures['serving_default']
+
+    return model
+
+
+MODEL_NAME = 'trained_inference_graph'
+PATH_TO_LABELS = 'labelmap.pbtxt'
+
+category_index = label_map_util.create_category_index_from_labelmap(
+    PATH_TO_LABELS, use_display_name=True)
+
+detection_model = load_model(MODEL_NAME)
+print(detection_model.inputs)
+
+
+def run_inference_for_single_image(model, image):
+    image = np.asarray(image)
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # Run inference
+    output_dict = model(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(output_dict.pop('num_detections'))
+    output_dict = {key: value[0, :num_detections].numpy()
+                   for key, value in output_dict.items()}
+    output_dict['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    output_dict['detection_classes'] = output_dict['detection_classes'].astype(
+        np.int64)
+
+    # Handle models with masks:
+    if 'detection_masks' in output_dict:
+        # Reframe the the bbox mask to the image size.
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            output_dict['detection_masks'], output_dict['detection_boxes'],
+            image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            detection_masks_reframed > 0.5, tf.uint8)
+        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+    return output_dict
+
+
+cap = cv2.VideoCapture(0)
+
+
+def run_inference(model, cap):
+    while True:
+        ret, image_np = cap.read()
+        # Actual detection.
+        output_dict = run_inference_for_single_image(model, image_np)
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks_reframed', None),
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        cv2.imshow('object_detection', cv2.resize(image_np, (800, 600)))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+
+run_inference(detection_model, cap)
--- local_inference_test.py
+++ local_inference_test.py
@@ -37,7 +37,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--cpu", help="Force to use CPU during inference.", action="store_true"
     )
-    parser.add_argument("--img", help="Path to a sample image to inference.", type=str)
+    parser.add_argument(
+        "--img", help="Path to a sample image to inference.", type=str)
     args = parser.parse_args()
 
     # Path to frozen detection graph. This is the actual model that is used for the object detection.
@@ -59,20 +60,21 @@ if __name__ == "__main__":
     def load_image_into_numpy_array(image):
         (im_width, im_height) = image.size
         return (
-            np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+            np.array(image.getdata()).reshape(
+                (im_height, im_width, 3)).astype(np.uint8)
         )
 
     def run_inference_benchmark(image, graph, trial=20, gpu=True):
         """Run TensorFlow inference benchmark.
-        
+
         Arguments:
             image {np.array} -- Input image as an Numpy array.
             graph {tf.Graph} -- TensorFlow graph object.
-        
+
         Keyword Arguments:
             trial {int} -- Number of inference to run for averaging. (default: {20})
             gpu {bool} -- Use Nvidia GPU when available. (default: {True})
-        
+
         Returns:
             int -- Frame per seconds benchmark result.
         """
@@ -85,7 +87,8 @@ if __name__ == "__main__":
             with tf.Session(config=config) as sess:
                 # Get handles to input and output tensors
                 ops = tf.get_default_graph().get_operations()
-                all_tensor_names = {output.name for op in ops for output in op.outputs}
+                all_tensor_names = {
+                    output.name for op in ops for output in op.outputs}
                 tensor_dict = {}
                 for key in [
                     "num_detections",
@@ -101,8 +104,10 @@ if __name__ == "__main__":
                         )
                 if "detection_masks" in tensor_dict:
                     # The following processing is only for single image
-                    detection_boxes = tf.squeeze(tensor_dict["detection_boxes"], [0])
-                    detection_masks = tf.squeeze(tensor_dict["detection_masks"], [0])
+                    detection_boxes = tf.squeeze(
+                        tensor_dict["detection_boxes"], [0])
+                    detection_masks = tf.squeeze(
+                        tensor_dict["detection_masks"], [0])
                     # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
                     real_num_detection = tf.cast(
                         tensor_dict["num_detections"][0], tf.int32
@@ -111,7 +116,8 @@ if __name__ == "__main__":
                         detection_boxes, [0, 0], [real_num_detection, -1]
                     )
                     detection_masks = tf.slice(
-                        detection_masks, [0, 0, 0], [real_num_detection, -1, -1]
+                        detection_masks, [0, 0, 0], [
+                            real_num_detection, -1, -1]
                     )
                     detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
                         detection_masks, detection_boxes, image.shape[0], image.shape[1]
@@ -131,12 +137,14 @@ if __name__ == "__main__":
                 times = []
                 # Kick start the first inference which takes longer and followings.
                 output_dict = sess.run(
-                    tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)}
+                    tensor_dict, feed_dict={
+                        image_tensor: np.expand_dims(image, 0)}
                 )
                 for i in range(trial):
                     start_time = time.time()
                     output_dict = sess.run(
-                        tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)}
+                        tensor_dict, feed_dict={
+                            image_tensor: np.expand_dims(image, 0)}
                     )
                     delta = time.time() - start_time
                     times.append(delta)
@@ -153,4 +161,5 @@ if __name__ == "__main__":
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
     # Actual detection benchmark.
-    fps = run_inference_benchmark(image_np, detection_graph, trial=20, gpu=not args.cpu)
+    fps = run_inference_benchmark(
+        image_np, detection_graph, trial=20, gpu=not args.cpu)
